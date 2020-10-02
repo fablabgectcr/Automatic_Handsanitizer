7cd2c23e1c71dc6be2c9fcb79838fd7e53143197
tcr17me038 <72256320+tcr17me038@users.noreply.github.com>
2020-10-02 16:46:12 +0530

    Update hand_sanit_HSR.ino

--- hand_sanit_HSR.ino
+++ hand_sanit_HSR.ino
@@ -1,21 +1,36 @@
+int const trigPin = 10;
+int const echoPin = 9;
+int const buzzPin = 2;
 
-const int servo = 9;
-const int IRSensor=8; 
+void setup()
+{
+	pinMode(trigPin, OUTPUT); // trig pin will have pulses output
+	pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
+	pinMode(buzzPin, OUTPUT); // buzz pin is output to control buzzering
+}
 
-// defines variables
-
-#include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  pinMode(IRSensor, INPUT);
-  myservo.attach(servo);  // attaches the servo on pin 9 to the servo object  
-  myservo.write(0);   // Sets Servo to initially 0 degrees 
-  Serial.begin(9600); // Starts the serial communication
+void loop()
+{
+	// Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+	int duration, distance;
+	// Output pulse with 1ms width on trigPin
+	digitalWrite(trigPin, HIGH); 
+	delay(1);
+	digitalWrite(trigPin, LOW);
+	// Measure the pulse input in echo pin
+	duration = pulseIn(echoPin, HIGH);
+	// Distance is half the duration devided by 29.1 (from datasheet)
+	distance = (duration/2) / 29.1;
+	// if distance less than 0.5 meter and more than 0 (0 or less means over range) 
+    if (distance <= 50 && distance >= 0) {
+    	// Buzz
+    	digitalWrite(buzzPin, HIGH);
+    } else {
+    	// Don't buzz
+    	digitalWrite(buzzPin, LOW);
+    }
+    // Waiting 60 ms won't hurt any one
+    delay(60);
 }
 
 void loop() {
